d745ad7e6a18bab87448c0e9c5ad43fb3279b89d
SEBonduel <sebastien050599@hotmail.fr>
2023-03-27 15:57:48 +0200

    push provider

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
+  //   MultiProvider(
+  //     providers: [
+        
+  //     ],
+  //     child: MyApp(),
+  //   ),
+  // );
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Reunionou',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Reuinonou'),
     );
   }
 }
