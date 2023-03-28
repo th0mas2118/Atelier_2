d60b864ecedc384d24e79318775af3dcd1b9824a
anthod57 <87667439+anthod57@users.noreply.github.com>
2023-03-28 16:43:32 +0200

    added colors

--- reunionou/tailwind.config.js
+++ reunionou/tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./index.html",
@@ -6,6 +9,15 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      purple: '#905ff4',
+      white: '#ffffff',
+      black: '#2b2b2b',
+      white2: '#f6f6f6',
+      gray: '#adadad',
+    }
   },
   plugins: [],
 }
