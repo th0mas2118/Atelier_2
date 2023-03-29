2f57ebd3f8b29612118d4c15b7b517c3889cf021
th0mas2118 <thomaspierson32@gmail.com>
2023-03-29 19:01:50 +0200

    rebase auth action front

--- reunionou/front_office_webapp/src/actions/auth/DeleteFriendAction.php
+++ reunionou/front_office_webapp/src/actions/auth/DeleteFriendAction.php
@@ -1,25 +1,27 @@
 <?php
 
-namespace reunionou\frontwebapp\actions;
+namespace reunionou\frontwebapp\actions\auth;
 
 use GuzzleHttp\Client;
 use Slim\Psr7\Request;
 use Slim\Psr7\Response;
 
-final class GetFriendsList
+final class DeleteFriendAction
 {
     public function __invoke(Request $rq, Response $rs, array $args): Response
     {
         $client  = new Client(['base_uri' => 'http://api.auth.reunionou'], ['timeout' => 2.0]);
+
         $id = $args['id'];
-        if ($id == null) {
+        $friend_id = $args['friend_id'];
+        if ($id == null || $friend_id == null) {
             throw new \Exception("id is null");
         }
 
+        $url = "/user/" . $id . "/friends/" . $friend_id;
 
-        $url = "/user/" . $id . "/friends";
+        $response = $client->request('DELETE', $url);
 
-        $response = $client->request('GET', $url);
-        return $rs->withStatus($response->getStatusCode())->withHeader('Content-Type', $response->getHeader('Content-Type'))->withBody($response->getBody());
+        return $rs->withStatus($response->getStatusCode());
     }
 }
---
+++ reunionou/front_office_webapp/src/actions/auth/GetUsersAction.php
@@ -0,0 +1,3 @@
+<?php
+
+namespace reunionou\frontwebapp\actions\auth;
