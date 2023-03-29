debd4a27c79b2289be5bc42dd9b9f323e7330d9e
th0mas2118 <thomaspierson32@gmail.com>
2023-03-29 09:16:19 +0200

    fix last

--- reunionou/src/views/LoginView.vue
+++ reunionou/src/views/LoginView.vue
@@ -9,33 +9,35 @@ let log = reactive({
 </script>
 
 <template>
-  <div class="login-view mt-8">
-    <form class="shadow-3xl rounded px-16 pt-16 pb-8 mb-8" type="login" onsubmit="return false"
-      @submit="user.setConnected(log)" @keyup.enter="user.setConnected(log)">
-      <h1 class="text-3xl font-bold mb-8 text-center">Se Connecter</h1>
-      <div class="mb-4">
-        <label class="block text-white-700 text-sm font-bold mb-2" for="username"> Username </label>
-        <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          type="email" placeholder="Email" v-model="log.email" />
-      </div>
-      <div class="mb-6">
-        <label class="block text-white-700 text-sm font-bold mb-2" for="password"> Password </label>
-        <input
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          type="password" placeholder="Password" v-model="log.password" />
-      </div>
-      <div class="flex items-center justify-between">
-        <button
-          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          type="button" @click="user.setConnected(log)">
-          Login
-        </button>
-        <router-link to="/register"
-          class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">S'inscrire ?
-        </router-link>
-      </div>
-    </form>
+  <div class="login-container flex w-full justify-center">
+    <div class="login-view mt-8">
+      <form class="shadow-3xl rounded px-16 pt-16 pb-8 mb-8" type="login" onsubmit="return false"
+        @submit="user.setConnected(log)" @keyup.enter="user.setConnected(log)">
+        <h1 class="text-3xl font-bold mb-8 text-center">Se Connecter</h1>
+        <div class="mb-4">
+          <label class="block text-white-700 text-sm font-bold mb-2" for="username"> Username </label>
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="email" placeholder="Email" v-model="log.email" />
+        </div>
+        <div class="mb-6">
+          <label class="block text-white-700 text-sm font-bold mb-2" for="password"> Password </label>
+          <input
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="password" placeholder="Password" v-model="log.password" />
+        </div>
+        <div class="flex items-center justify-between">
+          <button
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button" @click="user.setConnected(log)">
+            Login
+          </button>
+          <router-link to="/register"
+            class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">S'inscrire ?
+          </router-link>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
--- reunionou/src/views/RegisterView.vue
+++ reunionou/src/views/RegisterView.vue
@@ -40,86 +40,52 @@ const Register = async () => {
 </script>
 
 <template>
-  <div class="register-view">
-    <!-- <h1>Register</h1>
-        <form type="register" onsubmit="return false" @submit="Register()">
-            <input type="text" placeholder="Username" v-model="log.username" />
-            <input type="text" placeholder="Firstname" v-model="log.firstname" />
-            <input type="text" placeholder="Lastname" v-model="log.lastname" />
-            <input type="email" placeholder="Email" v-model="log.email" />
-            <input type="password" placeholder="Password" v-model="password.password" />
-            <input type="password" placeholder="Password confirmation" v-model="password.passwordConfirmation" />
-            <button @click="Register()">Register</button>
-        </form> -->
-
+  <div class="register-view flex w-full justify-center">
     <form class="shadow-xl max-w-xl mx-auto rounded px-16 pt-16 pb-8 mb-8">
       <h1 class="text-3xl font-bold mb-8 text-center">Register</h1>
       <div class="mb-4">
         <label class="block text-white-700 font-bold mb-2" for="username">Nom d'Utilisateur</label>
         <input
           class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          type="text"
-          placeholder="Username"
-          v-model="log.username"
-        />
+          type="text" placeholder="Username" v-model="log.username" />
       </div>
       <div class="mb-4">
         <label class="block text-white-700 font-bold mb-2" for="fistname">Prenom</label>
         <input
           class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          type="text"
-          placeholder="Firstname"
-          v-model="log.firstname"
-        />
+          type="text" placeholder="Firstname" v-model="log.firstname" />
       </div>
       <div class="mb-4">
         <label class="block text-white-700 font-bold mb-2" for="lastname">Nom</label>
         <input
           class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          type="text"
-          placeholder="Lastname"
-          v-model="log.lastname"
-        />
+          type="text" placeholder="Lastname" v-model="log.lastname" />
       </div>
       <div class="mb-4">
         <label class="block text-white-700 font-bold mb-2" for="email">Adresse email</label>
         <input
           class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          type="email"
-          placeholder="Email"
-          v-model="log.email"
-        />
+          type="email" placeholder="Email" v-model="log.email" />
       </div>
       <div class="mb-6">
         <label class="block text-white-700 font-bold mb-2" for="password">Mot de passe</label>
         <input
           class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          type="password"
-          placeholder="Password"
-          v-model="password.password"
-        />
+          type="password" placeholder="Password" v-model="password.password" />
       </div>
       <div class="mb-6">
         <label class="block text-white-700 font-bold mb-2" for="password">Confirmation mdp</label>
         <input
           class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          type="password"
-          placeholder="Password confirmation"
-          v-model="password.passwordConfirmation"
-        />
+          type="password" placeholder="Password confirmation" v-model="password.passwordConfirmation" />
       </div>
       <div class="flex items-center justify-between mt-11">
         <button
           class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          type="button"
-          @click="Register()"
-        >
+          type="button" @click="Register()">
           S'inscrire
         </button>
-        <router-link
-          to="/login"
-          class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-        >
+        <router-link to="/login" class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
           Déjà membre ?
         </router-link>
       </div>
@@ -131,6 +97,7 @@ const Register = async () => {
 form {
   justify-content: center;
 }
+
 /* @media (min-width: 1024px) {
   .register-view {
     min-height: 100vh;
