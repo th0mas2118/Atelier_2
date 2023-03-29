5482f50ddcabfaf7f1eda63da5a250efc0aaf940
SEBonduel <sebastien050599@hotmail.fr>
2023-03-29 17:19:43 +0200

    commit

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/screens/signup_screen.dart';
 import './screens/home_page.dart';
 import 'package:mobile/screens/profile_view_screen.dart';
 import 'package:provider/provider.dart';
+import './screens/signup_screen.dart';
 
 
 void main() {
@@ -14,19 +16,105 @@ void main() {
   //   ),
   // );
 }
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+ 
+  static const String _title = 'REUNIONOU';
+ 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Reunionou',
-      
-      theme: ThemeData(
-        primarySwatch: Colors.grey,
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const MyStatefulWidget(),
       ),
-      home: const MyHomePage(title: 'REUNIONOU'),
     );
+  }
+}
+ 
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+ 
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+ 
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  'Connexion',
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 30),
+                )),
+           
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Nom d\'utilisateur',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Mot de passe',
+                ),
+              ),
+            ),
+            
+            Container(
+                height: 100,
+                padding: const EdgeInsets.fromLTRB(200, 50, 200, 0),
+                child: ElevatedButton(
+                  child: const Text('Se connecter'),
+                  onPressed: () {
+                    print(nameController.text);
+                    print(passwordController.text);
+                  },
+                )
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text('Tu n\'as pas de compte ?'),
+                TextButton(
+                  child: const Text(
+                    'S\'inscrire',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const SignUpScreen()),
+                  );
+                  },
+                )
+              ],
+            ),
+          ],
+        ));
   }
 }
 
