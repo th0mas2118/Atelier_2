dc0d6c46fdae909950d0d6be5941552345dd058f
asley-hub <asleyvalisoa@gmail.com>
2023-03-29 14:46:42 +0200

    components chat & members

---
+++ reunionou/src/components/ChatBox.vue
@@ -0,0 +1,90 @@
+<template>
+<!-- <div
+      id="message-content"
+      class="w-full h-full flex min-h-[400px] justify-between items-start p-4 gap-2"
+    > -->
+      <div class="container mx-auto h-[60vh] flex flex-col">
+        <div class="flex-grow overflow-y-scroll px-4">
+          <div class="flex justify-end">
+            <div class="flex flex-col bg-cpurple rounded-lg list-inside mt-4 p-4 w-6/12">
+              <div class="flex items-center mb-4">
+                <div>
+                  <p class="leading-normal">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis orci erat.
+                    Ut nec justo sit amet velit auctor feugiat eu at ex. Sed feugiat nisi sed mauris
+                    laoreet euismod.orem Ipsum is simply dummy text of the printing and typesetting
+                    industry. Lorem Ipsum has been the industry's standard dummy text ever since the
+                    1500s, when an unknown printer took a galley of type and scrambled it to make a
+                    type specimen book. It has survived not only five centuries, but also the leap
+                    into electronic typesetting, remaining essentially unchanged. It was popularised
+                    in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+                    passages, and more recently with desktop publishing software like Aldus
+                    PageMaker including versions of Lorem Ipsum.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <img
+              class="w-12 h-12 object-cover rounded-full mr-4 self-end ml-4"
+              src="https://images.unsplash.com/photo-1519681393784-d120267933ba"
+              alt="avatar"
+            />
+          </div>
+          <div class="flex justify-start">
+            <img
+              class="w-12 h-12 object-cover rounded-full mr-4 self-end ml-4"
+              src="https://images.unsplash.com/photo-1519681393784-d120267933ba"
+              alt="avatar"
+            />
+            <div class="flex flex-col bg-gray-100 rounded-lg list-inside mt-4 p-4 w-6/12">
+              <div class="flex items-center mb-4">
+                <div>
+                  <p class="leading-normal">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis orci erat.
+                    Ut nec justo sit amet velit auctor feugiat eu at ex. Sed feugiat nisi sed mauris
+                    laoreet euismod.orem Ipsum is simply dummy text of the printing and typesetting
+                    industry. Lorem Ipsum has been the industry's standard dummy text ever since the
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="flex justify-start">
+            <img
+              class="w-12 h-12 object-cover rounded-full mr-4 self-end ml-4"
+              src="https://images.unsplash.com/photo-1519681393784-d120267933ba"
+              alt="avatar"
+            />
+            <div class="flex flex-col bg-gray-100 rounded-lg list-inside mt-4 p-4 w-6/12">
+              <div class="flex items-center mb-4">
+                <div>
+                  <p class="leading-normal">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis orci erat.
+                    Ut nec justo sit amet velit auctor feugiat eu at ex. Sed feugiat nisi sed mauris
+                    laoreet euismod.orem Ipsum is simply dummy text of the printing and typesetting
+                    industry. Lorem Ipsum has been the industry's standard dummy text ever since the
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <form>
+          <div class="flex items-center border-b border-cpurple py-2">
+            <input
+              class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+              type="text"
+              placeholder="ECRIVEZ VOTRE MESSAGE ..."
+              aria-label="Full name"
+            />
+            <button
+              class="flex-shrink-0 bg-cpurple hover:bg-purple-700 hover:border-purple-700 text-sm text-white py-3 px-4 rounded"
+              type="button"
+            >
+              Envoyer
+            </button>
+          </div>
+        </form>
+      </div>
+    <!-- </div> -->
+</template>
---
+++ reunionou/src/components/Members.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="members-list" class="max-h-[500px] overflow-y-auto">
+    <h3 class="text-cpurple font-bold min-w-[250px] mb-4">Members</h3>
+    <ul class="flex flex-col gap-2 overflow-auto">
+      <li class="flex items-center gap-2">
+        <div
+          class="w-12 h-12 rounded-full bg-cwhite2 flex items-center justify-center text-2xl transition-all hover:text-3xl duration-300 cursor-default overflow-hidden"
+        >
+          <i class="fa fa-plus" aria-hidden="true"></i>
+        </div>
+        <p>Add members</p>
+      </li>
+      <li class="flex items-center gap-2">
+        <div
+          class="w-12 h-12 rounded-full bg-cwhite2 flex items-center justify-center text-2xl transition-all hover:text-3xl duration-300 cursor-default overflow-hidden"
+        >
+          <img
+            src="https://www.gala.fr/imgre/fit/~1~gal~2021~08~02~ddad7c39-59ab-49d3-ba37-04f203f8029c.jpeg/480x480/quality/80/focus-point/757%2C593/photos-emmanuel-macron-en-t-shirt-a-bregancon-pas-le-1er-president-a-se-lacher-en-vacances.jpg"
+            alt=""
+            srcset=""
+          />
+        </div>
+        <p>John Doe</p>
+      </li>
+      <li class="flex items-center gap-2">
+        <div
+          class="w-12 h-12 rounded-full bg-cwhite2 flex items-center justify-center text-2xl transition-all hover:text-3xl duration-300 cursor-default overflow-hidden"
+        >
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/2/2d/Brigitte_Macron_%28July_2017%29_%28cropped%29.JPG"
+            alt=""
+            srcset=""
+          />
+        </div>
+        <p>John Doe</p>
+      </li>
+      <li class="flex items-center gap-2">
+        <div
+          class="w-12 h-12 rounded-full bg-cwhite2 flex items-center justify-center text-2xl transition-all hover:text-3xl duration-300 cursor-default overflow-hidden"
+        >
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/2/2d/Brigitte_Macron_%28July_2017%29_%28cropped%29.JPG"
+            alt=""
+            srcset=""
+          />
+        </div>
+        <p>John Doe</p>
+      </li>
+      <li class="flex items-center gap-2">
+        <div
+          class="w-12 h-12 rounded-full bg-cwhite2 flex items-center justify-center text-2xl transition-all hover:text-3xl duration-300 cursor-default overflow-hidden"
+        >
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/2/2d/Brigitte_Macron_%28July_2017%29_%28cropped%29.JPG"
+            alt=""
+            srcset=""
+          />
+        </div>
+        <p>John Doe</p>
+      </li>
+      <li class="flex items-center gap-2">
+        <div
+          class="w-12 h-12 rounded-full bg-cwhite2 flex items-center justify-center text-2xl transition-all hover:text-3xl duration-300 cursor-default overflow-hidden"
+        >
+          <img
+            src="https://www.gala.fr/imgre/fit/~1~gal~2021~08~02~ddad7c39-59ab-49d3-ba37-04f203f8029c.jpeg/480x480/quality/80/focus-point/757%2C593/photos-emmanuel-macron-en-t-shirt-a-bregancon-pas-le-1er-president-a-se-lacher-en-vacances.jpg"
+            alt=""
+            srcset=""
+          />
+        </div>
+        <p>John Doe</p>
+      </li>
+    </ul>
+  </div>
+</template>
