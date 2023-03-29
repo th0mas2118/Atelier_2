4e95f1bacf82f6f7206644a2c77c28b19b919e55
anthod57 <87667439+anthod57@users.noreply.github.com>
2023-03-29 11:35:12 +0200

    added page switch in event component

--- reunionou/src/components/EventInfo.vue
+++ reunionou/src/components/EventInfo.vue
@@ -7,7 +7,7 @@ const showOtherMenu = ref(false)
 
 <template lang="">
   <div
-    class="bg-cwhite text-cblack flex flex-col min-h-xl justify-center items-start w-full h-full m-4 rounded-3xl shadow-lg text-cblack"
+    class="bg-cwhite text-cblack flex flex-col min-h-[600px] justify-start items-center w-full h-full m-4 rounded-3xl shadow-lg text-cblack overflow-x-hidden"
   >
     <header
       class="p-8 w-full h-full flex justify-between items-center border-solid border-b-2 border-cwhite2"
@@ -67,101 +67,120 @@ const showOtherMenu = ref(false)
         </button>
       </div>
     </header>
+
     <div
-      id="event-content"
-      class="w-full h-full flex min-h-[400px] justify-between items-start p-4 gap-2"
+      id="content"
+      class="h-full w-full flex flex-grow min-h-[400px] transition-all duration-500 ease-in-out"
+      :style="{ transform: `translateX(-` + currentView * 100 + `%)` }"
     >
-      <div id="event-description" class="w-2/4">
-        <h3 class="text-cpurple font-bold min-w-[250px] mb-4">Description</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque omnis perferendis
-          doloremque ex ratione officiis, odio nulla ipsum iusto delectus cupiditate in debitis
-          aperiam libero recusandae maxime quibusdam. Mollitia, consectetur! Lorem, ipsum dolor sit
-          amet consectetur adipisicing elit. Accusamus quas aspernatur est fugiat, vero harum.
-          Officiis recusandae libero, ipsa nesciunt voluptatem consequatur repudiandae voluptas
-          facere ullam rerum quis iste optio.
-        </p>
-      </div>
-      <div id="participant-list" class="max-h-[500px] overflow-y-auto">
-        <h3 class="text-cpurple font-bold min-w-[250px] mb-4">Invités</h3>
-        <ul class="flex flex-col gap-2 overflow-auto">
-          <li class="flex items-center gap-2">
-            <div
-              class="w-12 h-12 rounded-full bg-cwhite2 flex items-center justify-center text-2xl transition-all hover:text-3xl duration-300 cursor-default overflow-hidden"
-            >
-              <img
-                src="https://www.gala.fr/imgre/fit/~1~gal~2021~08~02~ddad7c39-59ab-49d3-ba37-04f203f8029c.jpeg/480x480/quality/80/focus-point/757%2C593/photos-emmanuel-macron-en-t-shirt-a-bregancon-pas-le-1er-president-a-se-lacher-en-vacances.jpg"
-                alt=""
-                srcset=""
-              />
-            </div>
-            <p>John Doe</p>
-            <div>
-              <i class="fa-solid fa-check-circle text-cgreen"></i>
-            </div>
-          </li>
-          <li class="flex items-center gap-2">
-            <div
-              class="w-12 h-12 rounded-full bg-cwhite2 flex items-center justify-center text-2xl transition-all hover:text-3xl duration-300 cursor-default overflow-hidden"
-            >
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/2/2d/Brigitte_Macron_%28July_2017%29_%28cropped%29.JPG"
-                alt=""
-                srcset=""
-              />
-            </div>
-            <p>John Doe</p>
-            <div>
-              <i class="fa-solid fa-xmark-circle text-cred"></i>
-            </div>
-          </li>
-          <li class="flex items-center gap-2">
-            <div
-              class="w-12 h-12 rounded-full bg-cwhite2 flex items-center justify-center text-2xl transition-all hover:text-3xl duration-300 cursor-default overflow-hidden"
-            >
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/2/2d/Brigitte_Macron_%28July_2017%29_%28cropped%29.JPG"
-                alt=""
-                srcset=""
-              />
-            </div>
-            <p>John Doe</p>
-            <div>
-              <i class="fa-solid fa-xmark-circle text-cred"></i>
-            </div>
-          </li>
-          <li class="flex items-center gap-2">
-            <div
-              class="w-12 h-12 rounded-full bg-cwhite2 flex items-center justify-center text-2xl transition-all hover:text-3xl duration-300 cursor-default overflow-hidden"
-            >
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/2/2d/Brigitte_Macron_%28July_2017%29_%28cropped%29.JPG"
-                alt=""
-                srcset=""
-              />
-            </div>
-            <p>John Doe</p>
-            <div>
-              <i class="fa-solid fa-xmark-circle text-cred"></i>
-            </div>
-          </li>
-          <li class="flex items-center gap-2">
-            <div
-              class="w-12 h-12 rounded-full bg-cwhite2 flex items-center justify-center text-2xl transition-all hover:text-3xl duration-300 cursor-default overflow-hidden"
-            >
-              <img
-                src="https://www.gala.fr/imgre/fit/~1~gal~2021~08~02~ddad7c39-59ab-49d3-ba37-04f203f8029c.jpeg/480x480/quality/80/focus-point/757%2C593/photos-emmanuel-macron-en-t-shirt-a-bregancon-pas-le-1er-president-a-se-lacher-en-vacances.jpg"
-                alt=""
-                srcset=""
-              />
-            </div>
-            <p>John Doe</p>
-            <div>
-              <i class="fa-solid fa-check-circle text-cgreen"></i>
-            </div>
-          </li>
-        </ul>
-      </div>
+      <section
+        id="event-info"
+        class="w-full h-full flex justify-between items-start p-4 gap-2 flex-shrink-0"
+      >
+        <div id="event-description" class="w-2/4">
+          <h3 class="text-cpurple font-bold min-w-[250px] mb-4">Description</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque omnis perferendis
+            doloremque ex ratione officiis, odio nulla ipsum iusto delectus cupiditate in debitis
+            aperiam libero recusandae maxime quibusdam. Mollitia, consectetur! Lorem, ipsum dolor
+            sit amet consectetur adipisicing elit. Accusamus quas aspernatur est fugiat, vero harum.
+            Officiis recusandae libero, ipsa nesciunt voluptatem consequatur repudiandae voluptas
+            facere ullam rerum quis iste optio.
+          </p>
+        </div>
+        <div id="participant-list" class="max-h-[500px] overflow-y-auto">
+          <h3 class="text-cpurple font-bold min-w-[250px] mb-4">Invités</h3>
+          <ul class="flex flex-col gap-2 overflow-auto">
+            <li class="flex items-center gap-2">
+              <div
+                class="w-12 h-12 rounded-full bg-cwhite2 flex items-center justify-center text-2xl transition-all hover:text-3xl duration-300 cursor-default overflow-hidden"
+              >
+                <img
+                  src="https://www.gala.fr/imgre/fit/~1~gal~2021~08~02~ddad7c39-59ab-49d3-ba37-04f203f8029c.jpeg/480x480/quality/80/focus-point/757%2C593/photos-emmanuel-macron-en-t-shirt-a-bregancon-pas-le-1er-president-a-se-lacher-en-vacances.jpg"
+                  alt=""
+                  srcset=""
+                />
+              </div>
+              <p>John Doe</p>
+              <div>
+                <i class="fa-solid fa-check-circle text-cgreen"></i>
+              </div>
+            </li>
+            <li class="flex items-center gap-2">
+              <div
+                class="w-12 h-12 rounded-full bg-cwhite2 flex items-center justify-center text-2xl transition-all hover:text-3xl duration-300 cursor-default overflow-hidden"
+              >
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/2/2d/Brigitte_Macron_%28July_2017%29_%28cropped%29.JPG"
+                  alt=""
+                  srcset=""
+                />
+              </div>
+              <p>John Doe</p>
+              <div>
+                <i class="fa-solid fa-xmark-circle text-cred"></i>
+              </div>
+            </li>
+            <li class="flex items-center gap-2">
+              <div
+                class="w-12 h-12 rounded-full bg-cwhite2 flex items-center justify-center text-2xl transition-all hover:text-3xl duration-300 cursor-default overflow-hidden"
+              >
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/2/2d/Brigitte_Macron_%28July_2017%29_%28cropped%29.JPG"
+                  alt=""
+                  srcset=""
+                />
+              </div>
+              <p>John Doe</p>
+              <div>
+                <i class="fa-solid fa-xmark-circle text-cred"></i>
+              </div>
+            </li>
+            <li class="flex items-center gap-2">
+              <div
+                class="w-12 h-12 rounded-full bg-cwhite2 flex items-center justify-center text-2xl transition-all hover:text-3xl duration-300 cursor-default overflow-hidden"
+              >
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/2/2d/Brigitte_Macron_%28July_2017%29_%28cropped%29.JPG"
+                  alt=""
+                  srcset=""
+                />
+              </div>
+              <p>John Doe</p>
+              <div>
+                <i class="fa-solid fa-xmark-circle text-cred"></i>
+              </div>
+            </li>
+            <li class="flex items-center gap-2">
+              <div
+                class="w-12 h-12 rounded-full bg-cwhite2 flex items-center justify-center text-2xl transition-all hover:text-3xl duration-300 cursor-default overflow-hidden"
+              >
+                <img
+                  src="https://www.gala.fr/imgre/fit/~1~gal~2021~08~02~ddad7c39-59ab-49d3-ba37-04f203f8029c.jpeg/480x480/quality/80/focus-point/757%2C593/photos-emmanuel-macron-en-t-shirt-a-bregancon-pas-le-1er-president-a-se-lacher-en-vacances.jpg"
+                  alt=""
+                  srcset=""
+                />
+              </div>
+              <p>John Doe</p>
+              <div>
+                <i class="fa-solid fa-check-circle text-cgreen"></i>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <section
+        id="event-map"
+        class="w-full h-full flex justify-between items-start p-4 gap-2 flex-shrink-0"
+      >
+        MAP
+      </section>
+      <section
+        id="event-meteo"
+        class="w-full h-full flex justify-between items-start p-4 gap-2 flex-shrink-0"
+      >
+        METEO
+      </section>
     </div>
   </div>
 </template>
