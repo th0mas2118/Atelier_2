ab64b3f804eaaf63baaf168e35a56ff5cefa28bf
anthod57 <87667439+anthod57@users.noreply.github.com>
2023-03-29 17:16:06 +0200

    createventform

--- reunionou/src/components/CreateEventForm.vue
+++ reunionou/src/components/CreateEventForm.vue
@@ -1,13 +1,64 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onMounted, onUnmounted, reactive, ref } from 'vue'
+import EmojiPicker from 'vue3-emoji-picker'
+import 'vue3-emoji-picker/css'
 
 const currentPage = ref(0)
+const showEmojiPicker = ref(false)
+
+const setPage = (page: number) => {
+  if (page == 1) {
+    if (
+      eventData.title == '' ||
+      eventData.address == '' ||
+      eventData.date == '' ||
+      eventData.description == ''
+    ) {
+      return
+    }
+  }
+  currentPage.value = page
+  showEmojiPicker.value = false
+}
+
+const setEmoji = (emoji: string) => {
+  eventData.icon = emoji.i
+  showEmojiPicker.value = false
+}
+
+const listenClick = (e: MouseEvent) => {
+  const target = e.target as HTMLInputElement
+
+  if (target && target.id !== 'event-icon') {
+    showEmojiPicker.value = false
+  }
+}
+
+const eventData = reactive({
+  title: '',
+  date: '',
+  address: '',
+  gps: [],
+  description: '',
+  icon: '',
+  isPrivate: false,
+  organizer_id: '',
+  participants: []
+})
+
+onMounted(() => {
+  window.addEventListener('click', listenClick)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('click', listenClick)
+})
 </script>
 
 <template>
   <form
     action=""
-    class="bg-cwhite text-cblack flex flex-col min-h-[600px] justify-start items-center max-w-[800px] w-full h-full m-auto rounded-3xl shadow-lg text-cblack overflow-x-hidden"
+    class="bg-cwhite text-cblack flex flex-col min-h-[600px] justify-start items-center max-w-[800px] w-full h-full m-auto md:rounded-3xl shadow-lg text-cblack overflow-x-hidden"
   >
     <h1 class="text-cblack font-bold min-w-[250px] text-2xl w-full text-center p-4">
       CRÉER UN EVENEMENT
@@ -22,20 +73,30 @@ const currentPage = ref(0)
             <div>
               <label class="block text-white-700 text-sm font-bold mb-2" for="titre">Titre</label>
               <input
-                class="shadow appearance-none border-[#E5E7EB] rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:ring-transparent focus:border-[#E5E7EB]"
+                :class="`${
+                  'shadow appearance-none ' +
+                  (eventData.title == '' ? 'border-cred' : 'border-[#E5E7EB]') +
+                  ' rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:ring-transparent focus:border-[#E5E7EB]'
+                }`"
                 name="titre"
                 id="titre"
                 type="text"
+                v-model="eventData.title"
                 placeholder="Titre"
               />
             </div>
             <div>
               <label class="block text-white-700 text-sm font-bold mb-2" for="date">Date</label>
               <input
-                class="shadow appearance-none border-[#E5E7EB] rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:ring-transparent focus:border-[#E5E7EB]"
+                :class="`${
+                  'shadow appearance-none ' +
+                  (eventData.date == '' ? 'border-cred' : 'border-[#E5E7EB]') +
+                  ' rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:ring-transparent focus:border-[#E5E7EB]'
+                }`"
                 name="date"
                 id="date"
                 type="datetime-local"
+                v-model="eventData.date"
                 placeholder="Date"
               />
             </div>
@@ -46,10 +107,15 @@ const currentPage = ref(0)
                 Adresse
               </label>
               <input
-                class="shadow appearance-none border-[#E5E7EB] rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:ring-transparent focus:border-[#E5E7EB]"
+                :class="`${
+                  'shadow appearance-none ' +
+                  (eventData.address == '' ? 'border-cred' : 'border-[#E5E7EB]') +
+                  ' rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:ring-transparent focus:border-[#E5E7EB]'
+                }`"
                 name="adresse"
                 id="adresse"
                 type="text"
+                v-model="eventData.address"
                 placeholder="Adresse"
               />
             </div>
@@ -59,10 +125,15 @@ const currentPage = ref(0)
               >Description</label
             >
             <textarea
-              class="shadow appearance-none border-[#E5E7EB] rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:ring-transparent focus:border-[#E5E7EB]"
+              :class="`${
+                'shadow appearance-none ' +
+                (eventData.description == '' ? 'border-cred' : 'border-[#E5E7EB]') +
+                ' rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:ring-transparent focus:border-[#E5E7EB]'
+              }`"
               name="description"
               id="description"
               type="text"
+              v-model="eventData.description"
               placeholder="Description"
               rows="8"
             />
@@ -75,18 +146,124 @@ const currentPage = ref(0)
             <div>
               <input
                 checked
-                id="purple-checkbox"
+                id="isPrivate"
+                name="isPrivate"
                 type="checkbox"
                 value=""
-                class="w-4 h-4 text-purple-600 bg-gray-100 border-gray-300 rounded focus:ring-purple-500 dark:focus:ring-purple-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                v-model="eventData.isPrivate"
+                class="w-5 h-5 text-cpurple bg-gray-100 border-gray-300 rounded focus:ring-purple-500 dark:focus:ring-cpurple dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
               />
               <label
-                for="purple-checkbox"
+                for="isPrivate"
                 class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                >Purple</label
+                >Évenement privé</label
               >
             </div>
+            <div>
+              <h3 class="text-cpurple font-bold min-w-[250px] mb-4">Invités</h3>
+
+              <ul class="flex flex-col gap-2 overflow-auto max-h-[328px]">
+                <!-- <li class="flex items-center gap-2">
+                  <div
+                    class="w-10 h-10 rounded-full bg-cwhite2 flex items-center justify-center text-2xl transition-all hover:text-3xl duration-300 cursor-default overflow-hidden"
+                  >
+                    <img
+                      src="https://www.gala.fr/imgre/fit/~1~gal~2021~08~02~ddad7c39-59ab-49d3-ba37-04f203f8029c.jpeg/480x480/quality/80/focus-point/757%2C593/photos-emmanuel-macron-en-t-shirt-a-bregancon-pas-le-1er-president-a-se-lacher-en-vacances.jpg"
+                      alt=""
+                      srcset=""
+                    />
+                  </div>
+                  <p>John Doe</p>
+                  <div>
+                    <i class="fa-solid fa-xmark text-cred"></i>
+                  </div>
+                </li> -->
+              </ul>
+            </div>
           </div>
+          <div class="flex flex-col w-2/4 p-4 gap-4">
+            <div>
+              <header class="flex flex-col mb-4">
+                <h3 class="text-cpurple font-bold min-w-[250px] mb-4">Rechercher</h3>
+                <div class="relative">
+                  <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg
+                      aria-hidden="true"
+                      class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                      ></path>
+                    </svg>
+                  </div>
+                  <input
+                    type="search"
+                    id="default-search"
+                    class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-cpurple focus:border-cpurple dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-cpurple dark:focus:border-cpurple"
+                    placeholder="Nom, prénom, email, nom d'utilisateur"
+                    required
+                  />
+                </div>
+              </header>
+              <ul class="flex flex-col gap-2 overflow-auto max-h-[314px]">
+                <!-- <li class="flex items-center gap-2">
+                  <div
+                    class="w-10 h-10 rounded-full bg-cwhite2 flex items-center justify-center text-2xl transition-all hover:text-3xl duration-300 cursor-default overflow-hidden"
+                  >
+                    <img
+                      src="https://www.gala.fr/imgre/fit/~1~gal~2021~08~02~ddad7c39-59ab-49d3-ba37-04f203f8029c.jpeg/480x480/quality/80/focus-point/757%2C593/photos-emmanuel-macron-en-t-shirt-a-bregancon-pas-le-1er-president-a-se-lacher-en-vacances.jpg"
+                      alt=""
+                      srcset=""
+                    />
+                  </div>
+                  <p>John Doe</p>
+                  <div>
+                    <i class="fa-solid fa-plus text-cgreen"></i>
+                  </div>
+                </li> -->
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!--  -->
+      <section id="page3" class="flex-shrink-0 w-full h-full p-4 relative">
+        <div id="summary" class="flex flex-col gap-4">
+          <div class="flex items-center gap-4 flex-col w-full sm:flex-row text-center md:text-left">
+            <div
+              id="event-icon"
+              class="w-16 h-16 rounded-full bg-cwhite2 flex items-center justify-center text-2xl aspect-square cursor-pointer"
+              @click="showEmojiPicker = true"
+            >
+              <i v-if="eventData.icon == ''" class="fa-solid fa-plus animate-bounce"></i>
+              {{ eventData.icon }}
+              <div
+                id="emoji-picker"
+                v-if="showEmojiPicker"
+                class="absolute m-auto left-0 right-0 top-1/2 transform -translate-y-1/2 z-10 w-[320px]"
+                @click="($event) => $event.stopPropagation()"
+              >
+                <EmojiPicker :native="true" @select="setEmoji" />
+              </div>
+            </div>
+            <div>
+              <h1 class="text-2xl">{{ eventData.title }}</h1>
+              <p class="text-md text-cgray">10 invités</p>
+            </div>
+          </div>
+          <span>Adresse: {{ eventData.address }}</span>
+          <span>Date: {{ eventData.date }}</span>
+          <span>Description: </span>
+          <p>
+            {{ eventData.description }}
+          </p>
         </div>
       </section>
     </div>
@@ -94,7 +271,7 @@ const currentPage = ref(0)
       <button
         type="button"
         v-if="currentPage > 0"
-        @click="currentPage--"
+        @click="setPage(currentPage - 1)"
         class="bg-cpurple hover:bg-[#9a69fe] text-cwhite py-2 px-4 rounded-3xl transition-all duration-300 overflow-hidden whitespace-nowrap"
       >
         Précedent
@@ -102,7 +279,7 @@ const currentPage = ref(0)
       <button
         type="button"
         v-if="currentPage < 2"
-        @click="currentPage++"
+        @click="setPage(currentPage + 1)"
         class="bg-cpurple hover:bg-[#9a69fe] text-cwhite py-2 px-4 rounded-3xl transition-all duration-300 overflow-hidden whitespace-nowrap self-end ml-auto"
       >
         Suivant
