67bf4dc31c7fbde49e81ce85702fe404c1fabc78
th0mas2118 <thomaspierson32@gmail.com>
2023-03-30 17:16:19 +0200

    add listinvitation

---
+++ reunionou/src/components/ListInvitation.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import axios from 'axios';
+import { useRoute } from 'vue-router';
+import router from '@/router';
+
+const invitations = ref({})
+
+const updateInvitStatus = (id: String, state: Boolean) => {
+    axios
+        .patch(`http://api.frontoffice.reunionou:49383/invitations/${id}`, {
+            accepted: state,
+        })
+}
+
+axios
+    .get(`http://api.frontoffice.reunionou:49383/user/${useRoute().params['id']}/invitations`)
+    .then((response) => invitations.value = response.data.invitations).then((response) => console.log(invitations.value[0]))
+</script>
+
+<template>
+    <div
+        class="invitation_list bg-cwhite text-cblack flex flex-col  justify-start items-center w-10/12 h-full m-4 rounded-3xl shadow-lg text-cblack overflow-x-hidden">
+        <div v-for="invit in invitations" class="m-8 flex flex-row justify-between w-11/12 ">
+
+            <div class="flex flex-col justify-center">
+                <div>Organisateur : {{ invit.organizer.username }}</div>
+                <div>Nom de l'événement : {{ invit.event_title }}</div>
+            </div>
+            <div class="flex flex-col gap-2">
+                <div class="bg-cgreen text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline text-center"
+                    v-if="invit.accepted == true">PARTICIPE</div>
+                <div class="bg-cred text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline text-center"
+                    v-if="invit.accepted == false">NE PARTICIPE PAS</div>
+                <div class="flex flex-row gap-3">
+                    <div
+                        :class="`${invit.accepted ? 'bg-cgray' : 'bg-cgreen hover:bg-blue-700 '}  text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline text-center`">
+                        <button :disabled="invit.accepted" @click="updateInvitStatus(invit.id, true)">Accepter</button>
+                    </div>
+                    <div
+                        :class="`${!invit.accepted ? 'bg-cgray' : 'bg-cred hover:bg-blue-700'}   text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline text-center`">
+                        <button :disabled="!invit.accepted" @click="updateInvitStatus(invit.id, false)">Refuser</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
--- reunionou/src/views/UserView.vue
+++ reunionou/src/views/UserView.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import UserInfo from '../components/UserInfo.vue'
 import UserFriendList from '../components/UserFriendList.vue'
+import ListInviation from '../components/ListInvitation.vue'
 
 </script>
 <template lang="" >
         <div class=" text-cblack flex flex-col justify-start items-center w-full h-full m-4">
             <UserInfo></UserInfo>
-            <UserFriendList></UserFriendList>
+            <ListInviation></ListInviation>
         </div>
 </template>
