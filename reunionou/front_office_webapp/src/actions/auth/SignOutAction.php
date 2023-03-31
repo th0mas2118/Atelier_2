a64e74423482533328c39723fd01241f9f109bb0
th0mas2118 <thomaspierson32@gmail.com>
2023-03-31 11:56:26 +0200

    add clear guzzle error

--- reunionou/front_office_webapp/src/actions/auth/SignOutAction.php
+++ reunionou/front_office_webapp/src/actions/auth/SignOutAction.php
@@ -5,6 +5,7 @@ namespace reunionou\frontwebapp\actions\auth;
 use GuzzleHttp\Client;
 use Slim\Psr7\Request;
 use Slim\Psr7\Response;
+use GuzzleHttp\Exception\RequestException;
 
 
 final class SignOutAction
@@ -13,7 +14,14 @@ final class SignOutAction
     {
         $client  = new Client(['base_uri' => 'http://api.auth.reunionou'], ['timeout' => 2.0]);
 
-        $response = $client->request('POST', '/signout', ['headers' => ['Authorization' => $rq->getHeader('Authorization')]]);
+        try {
+            $response = $client->request('POST', '/signout', ['headers' => ['Authorization' => $rq->getHeader('Authorization')]]);
+        } catch (RequestException $e) {
+            $rs->getBody()->write($e->getResponse()->getBody()->getContents());
+            return $rs->withStatus(400)->withHeader('Content-Type', 'application/json');
+        }
+
+
 
         return $rs->withStatus($response->getStatusCode())->withHeader('Content-Type', $response->getHeader('Content-Type'))->withBody($response->getBody());
     }
--- reunionou/front_office_webapp/src/actions/auth/SignUpAction.php
+++ reunionou/front_office_webapp/src/actions/auth/SignUpAction.php
@@ -5,6 +5,7 @@ namespace reunionou\frontwebapp\actions\auth;
 use GuzzleHttp\Client;
 use Slim\Psr7\Request;
 use Slim\Psr7\Response;
+use GuzzleHttp\Exception\RequestException;
 
 final class SignUpAction
 {
@@ -12,7 +13,12 @@ final class SignUpAction
     {
         $client  = new Client(['base_uri' => 'http://api.auth.reunionou'], ['timeout' => 2.0]);
 
-        $response = $client->request('POST', '/signup', ['body' => $rq->getBody()->getContents()]);
+        try {
+            $response = $client->request('POST', '/signup', ['body' => $rq->getBody()->getContents()]);
+        } catch (RequestException $e) {
+            $rs->getBody()->write($e->getResponse()->getBody()->getContents());
+            return $rs->withStatus(400)->withHeader('Content-Type', 'application/json');
+        }
 
         return $rs->withStatus($response->getStatusCode())->withHeader('Content-Type', $response->getHeader('Content-Type'))->withBody($response->getBody());
     }
