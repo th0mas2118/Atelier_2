1aaf9733933b24b9a2c2ecc75c372e2c7fe1e729
anthod57 <87667439+anthod57@users.noreply.github.com>
2023-04-02 16:21:29 +0200

    added organizer info

--- reunionou/src/components/EventInfo.vue
+++ reunionou/src/components/EventInfo.vue
@@ -19,6 +19,8 @@ const showCreateInvitationLinkPopup = ref(false)
 const isGuest = ref(false)
 const guestId = ref('')
 
+const avatarUrl = import.meta.env.VITE_API_HOST + '/avatars/'
+
 const getConfirmatedParticipantsCount = (): number => {
   return event.value.participants.filter((participant: any) => participant.status == 'confirmed')
     .length
@@ -237,14 +239,36 @@ onMounted(() => {
     >
       <section
         id="event-info"
-        class="w-full flex flex-col md:flex-row justify-between items-stretch p-4 gap-2 flex-shrink-0"
+        class="w-full flex flex-col md:flex-row md:justify-between items-stretch p-4 gap-4 md:gap-2 flex-shrink-0"
       >
-        <div id="event-description" class="w-full md:w-2/4">
-          <h3 class="text-cpurple font-bold min-w-[250px] mb-4">Description</h3>
-          <p>
-            {{ event.description }}
-          </p>
+        <div class="flex w-full md:w-2/4 h-auto md:h-full flex-col gap-4">
+          <div id="event-organizer" class="w-full md:w-2/4">
+            <h3 class="text-cpurple font-bold min-w-[250px] mb-4">Créateur</h3>
+            <li class="flex items-center gap-2">
+              <div
+                class="w-12 h-12 rounded-full bg-cwhite2 flex items-center justify-center text-2xl transition-all hover:text-3xl duration-300 cursor-default overflow-hidden"
+              >
+                <img
+                  :src="avatarUrl + event.organizer.id + '/80/80'"
+                  alt=""
+                  srcset=""
+                  class="w-full h-full"
+                />
+              </div>
+              <div class="flex flex-col">
+                <span>{{ event.organizer.firstname + ' ' + event.organizer.lastname }}</span>
+                <span>{{ event.organizer.email }}</span>
+              </div>
+            </li>
+          </div>
+          <div id="event-description" class="w-full md:w-2/4">
+            <h3 class="text-cpurple font-bold min-w-[250px] mb-4">Description</h3>
+            <p>
+              {{ event.description }}
+            </p>
+          </div>
         </div>
+
         <div id="participant-list" class="max-h-[500px] overflow-y-auto">
           <h3 class="text-cpurple font-bold min-w-[250px] mb-4">
             Invités ({{ event.participants.length }})
@@ -259,12 +283,13 @@ onMounted(() => {
                 class="w-12 h-12 rounded-full bg-cwhite2 flex items-center justify-center text-2xl transition-all hover:text-3xl duration-300 cursor-default overflow-hidden"
               >
                 <img
-                  src="https://www.gala.fr/imgre/fit/~1~gal~2021~08~02~ddad7c39-59ab-49d3-ba37-04f203f8029c.jpeg/480x480/quality/80/focus-point/757%2C593/photos-emmanuel-macron-en-t-shirt-a-bregancon-pas-le-1er-president-a-se-lacher-en-vacances.jpg"
+                  :src="avatarUrl + user.user.id + '/80/80'"
                   alt=""
                   srcset=""
+                  class="w-full h-full"
                 />
               </div>
-              <p>{{ user.user.firstname + ' ' + user.user.lastname }}</p>
+              <span>{{ user.user.firstname + ' ' + user.user.lastname }}</span>
               <div>
                 <i
                   v-if="user.status == 'confirmed'"
