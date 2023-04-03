9bc2222f608febc31ef3281eaad47eb9b4bd8133
SEBonduel <sebastien050599@hotmail.fr>
2023-04-03 17:27:01 +0200

    fix login

--- reunionou/src/views/HomeView.vue
+++ reunionou/src/views/HomeView.vue
@@ -17,7 +17,7 @@ const user = useUserStore()
               :to="{ name: 'login' }"
               active-class="bg-cpurple text-cwhite hover:text-cwhite"
               class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-cpurple rounded-lg hover:bg-[#9a69fe] focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-              >Se connecter
+              Se connecter
               <svg aria-hidden="true" class="w-6 h-6 ml-4 -mr-2" fill="currentColor" viewBox="0 0 30 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
           </router-link>
         </div>
@@ -30,7 +30,7 @@ const user = useUserStore()
             :to="{ name: 'register' }"
             active-class="bg-cpurple text-cwhite hover:text-cwhite"
             class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-cpurple rounded-lg hover:bg-[#9a69fe] focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-          >Créer un compte
+          Créer un compte
           <svg aria-hidden="true" class="w-6 h-6 ml-4 -mr-2" fill="currentColor" viewBox="0 0 30 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
         </RouterLink>
       </div>
