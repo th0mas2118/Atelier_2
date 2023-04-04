ba64e49b3c656c875ba037e1e33afe87b0f4291c
th0mas2118 <thomaspierson32@gmail.com>
2023-04-04 10:22:18 +0200

    sidebar to a new components

--- mobile/lib/Screens/MyPage/mypage_screen.dart
+++ mobile/lib/Screens/MyPage/mypage_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_auth/Screens/MyPage/components/my_info.dart';
 import 'package:flutter_auth/Screens/invit_list_screen.dart';
 import 'package:provider/provider.dart';
 import '../../../constants.dart';
+import '../../components/side_bar.dart';
 
 import '../../../provider/user_model.dart';
 import '../../class/invitations.dart';
@@ -25,73 +26,7 @@ class _MyPageState extends State<MyPage> {
           backgroundColor: kPrimaryColor,
           automaticallyImplyLeading: false,
         ),
-        endDrawer: Drawer(
-          child: ListView(
-            children: [
-              SizedBox(
-                height: 64,
-                child: DrawerHeader(
-                  decoration: const BoxDecoration(
-                    color: kPrimaryColor,
-                  ),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: kPrimaryLightColor,
-                    ),
-                    onPressed: () {
-                      Provider.of<UserModel>(context, listen: false)
-                          .logout(context);
-                    },
-                    child: const Text('Logout',
-                        style: TextStyle(
-                          color: Colors.black,
-                        )),
-                  ),
-                ),
-              ),
-              SizedBox(
-                width: 64,
-                child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            //Ou tu veux aller
-                            return const MyPage();
-                          },
-                        ),
-                      );
-                    },
-                    child: const Text('Créer un évènement')),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text(
-                    'Mes Invitations',
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
-                  ),
-                ],
-              ),
-              FutureBuilder(
-                future: Provider.of<UserModel>(context, listen: false)
-                    .getInvit(context),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return InvitListScreen(
-                        invitationsList: snapshot.data as List<Invitations>);
-                  } else if (snapshot.hasError) {
-                    return Text("${snapshot.error}");
-                  }
-                  return const Center(child: CircularProgressIndicator());
-                },
-              ),
-            ],
-          ),
-        ),
+        endDrawer: const SideBar(),
         body: Column(
           children: [
             const MyInfo(),
