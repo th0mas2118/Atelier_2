462cbb5116927444ec410d9694d55d6b50c13eea
SEBonduel <sebastien050599@hotmail.fr>
2023-04-05 13:02:40 +0200

    fix

--- mobile/lib/responsive.dart
+++ mobile/lib/responsive.dart
@@ -19,15 +19,11 @@ class Responsive extends StatelessWidget {
       MediaQuery.of(context).size.width >= 576 &&
       MediaQuery.of(context).size.width <= 992;
 
-  static bool isDesktop(BuildContext context) =>
-      MediaQuery.of(context).size.width > 992;
 
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
-    if (size.width > 992) {
-      return desktop;
-    } else if (size.width >= 576 && tablet != null) {
+    if (size.width >= 576 && tablet != null) {
       return tablet!;
     } else {
       return mobile;
